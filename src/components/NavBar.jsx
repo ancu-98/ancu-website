91d054094e121f26148b1b76cce032923f8e9b18
Ancu-98 <jdsebastianb30@hotmail.com>
2023-03-22 10:47:05 -0500

    navbar responsive changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import './styles/navBar.css'
 import './styles/responsive/tablet.css'
 import './styles/responsive/mobile.css'
 
-const NavBar = ({handleTab}) => {
+const NavBar = ({ handleTab }) => {
 
     return (
         <nav className='navBar__container-all'>
@@ -40,38 +40,41 @@ const NavBar = ({handleTab}) => {
 
             <div className='navBar__container-mobile'>
                 <div className='navBar__container-button-list' >
-                    <button className='button__item-li' onClick={() => handleTab(1)} >
-                        <a href="#" className='item-li'>
-                            <span className='icon'>
-                                <img src="/src/assets/img/home.svg" alt="logo_Home" />
-                            </span>
-                            <span className='text'>Home</span>
-                        </a>
-                    </button>
-                    <button className='button__item-li' onClick={() => handleTab(2)} >
-                        <a href="#" className='item-li'>
-                            <span className='icon'>
-                                <img src="/src/assets/img/user.svg" alt="logo_About" />
-                            </span>
-                            <span className='text'>About</span>
-                        </a>
-                    </button>
-                    <button className='button__item-li' onClick={() => handleTab(3)} >
-                        <a href="#" className='item-li'>
-                            <span className='icon'>
-                                <img src="/src/assets/img/briefcase-account.svg" alt="logo_proyects" />
-                            </span>
-                            <span className='text'>Proyects</span>
-                        </a>
-                    </button>
-                    <button className='button__item-li' onClick={() => handleTab(4)} >
-                        <a href="#" className='item-li'>
-                            <span className='icon'>
-                                <img src="/src/assets/img/mail-flash.svg" alt="logo_contact" />
-                            </span>
-                            <span className='text'>Contact</span>
-                        </a>
-                    </button>
+                    <ul>
+                        <li className='button__item-li active' onClick={() => handleTab(1)} >
+                            <a href="#" className='item-li'>
+                                <span className='icon'>
+                                    <img src="/src/assets/img/home.svg" alt="logo_Home" />
+                                </span>
+                                <span className='text'>Home</span>
+                            </a>
+                        </li>
+                        <li className='button__item-li' onClick={() => handleTab(2)} >
+                            <a href="#" className='item-li'>
+                                <span className='icon'>
+                                    <img src="/src/assets/img/user.svg" alt="logo_About" />
+                                </span>
+                                <span className='text'>About</span>
+                            </a>
+                        </li>
+                        <li className='button__item-li' onClick={() => handleTab(3)} >
+                            <a href="#" className='item-li'>
+                                <span className='icon'>
+                                    <img src="/src/assets/img/briefcase-account.svg" alt="logo_proyects" />
+                                </span>
+                                <span className='text'>Proyects</span>
+                            </a>
+                        </li>
+                        <li className='button__item-li' onClick={() => handleTab(4)} >
+                            <a href="#" className='item-li'>
+                                <span className='icon'>
+                                    <img src="/src/assets/img/mail-flash.svg" alt="logo_contact" />
+                                </span>
+                                <span className='text'>Contact</span>
+                            </a>
+                        </li>
+                        <div className='indicator'></div>
+                    </ul>
                     <label className='switch'>
                         <input type="checkbox" />
                         <span className='slider_nav'></span>
