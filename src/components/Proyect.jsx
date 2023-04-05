3f185ba73c57b0739ed62e13110bae453cbb0161
Ancu-98 <jdsebastianb30@hotmail.com>
2023-04-05 01:39:14 -0500

    responsive changes proyects section

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -7,47 +7,44 @@ import documentationIcon from '/src/assets/img/card-text.svg'
 const Proyect = ({ nameProyect, iconProyect, imgProyect, gitHubLink, deployLink, documentationLink, type, titleDescription, description }) => {
     return (
         <div className="card">
-            <div className="additional">
-
-                <div className='proyect-card'>
-                    <div className='proyect-name'>
-                        {nameProyect}
-                    </div>
-                    <div className='icon-container'>
-                        <img src={iconProyect} alt="icon-proyect" />
-                    </div>
-                    <div className="points">
-                        5,312 Points
-                    </div>
+            <div className='proyect-card'>
+                <div className='proyect-name'>
+                    {nameProyect}
                 </div>
+                <div className='icon-container'>
+                    <img src={iconProyect} alt="icon-proyect" />
+                </div>
+                <div className="points">
+                    5,312 Points
+                </div>
+            </div>
 
-                <div className="more-info">
-                    <div className="img-container">
-                        <img src={imgProyect} alt="img-proyect" />
+            <div className="more-info">
+                <div className="img-container">
+                    <img src={imgProyect} alt="img-proyect" />
+                </div>
+                <div className="stats_container">
+                    <div>
+                        <h3 className="title">Github</h3>
+                        <a className='stats_img' href={gitHubLink} target='_blank'>
+                            <img src={gitHubIcon} alt="github-icon" />
+                        </a>
                     </div>
-                    <div className="stats_container">
-                        <div>
-                            <h3 className="title">Github</h3>
-                            <a className='stats_img' href={gitHubLink} target='_blank'>
-                                <img src={gitHubIcon} alt="github-icon" />
-                            </a>
-                        </div>
-                        <div>
-                            <h3 className="title">Deploy</h3>
-                            <a className='stats_img' href={deployLink} target='_blank'>
-                                <img src={deployIcon} alt="deploy-icon" />
-                            </a>
-                        </div>
-                        <div>
-                            <h3 className="title">Docs</h3>
-                            <a className='stats_img' href={documentationLink} target='_blank'>
-                                <img src={documentationIcon} alt="doc-icon" />
-                            </a>
-                        </div>
-                        <div>
-                            <h3 className="title">Type</h3>
-                            <h4>{type}</h4>
-                        </div>
+                    <div>
+                        <h3 className="title">Deploy</h3>
+                        <a className='stats_img' href={deployLink} target='_blank'>
+                            <img src={deployIcon} alt="deploy-icon" />
+                        </a>
+                    </div>
+                    <div>
+                        <h3 className="title">Docs</h3>
+                        <a className='stats_img' href={documentationLink} target='_blank'>
+                            <img src={documentationIcon} alt="doc-icon" />
+                        </a>
+                    </div>
+                    <div>
+                        <h3 className="title">Type</h3>
+                        <h4>{type}</h4>
                     </div>
                 </div>
             </div>
@@ -57,7 +54,6 @@ const Proyect = ({ nameProyect, iconProyect, imgProyect, gitHubLink, deployLink,
                 <p>{description}</p>
                 <span className='more'>Hover the card for more info</span>
             </div>
-
         </div>
     )
 }
